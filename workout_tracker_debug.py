2b71b9f46bdafb8974b2786ef31b20355042775d
jasoncrawford31 <jasoncrawford31@gmail.com>
2025-04-04 11:51:05 -0400

    Add files via upload

--- workout_tracker_debug.py
+++ workout_tracker_debug.py
@@ -5,6 +5,8 @@ from datetime import date
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+st.write(Secrets loaded:" list(st.secrets.keys()))
+         
 
 def connect_to_gsheet():
     try:
